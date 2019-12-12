29321ec05bb62293753ba48280265857c900e78a
Jason Chang <jasonchang0@berkeley.edu>
2019-12-12 13:19:34 +0100

    removed duplicate .csv files

--- scripts/plot_abs_spectrum.py
+++ scripts/plot_abs_spectrum.py
@@ -25,4 +25,22 @@ os.chdir(db)
 df = pd.read_csv('ph_meta.csv')
 df.rename({'WL/nm': 'wl'}, axis=1, inplace=True)
 
+target_sample = 3
+
+sub_df = df[(df['rep'] == 1) & (df['sample'] == target_sample)].drop(['rep', 'sample'], axis=1, inplace=False)
+
+X = list(sub_df['wl'])
+Y = list(sub_df['timepoint'])
+Z = list(sub_df['Abs'])
+
+ax = plt.axes(projection='3d')
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
+                cmap='magma', edgecolor='none')
+
+ax.set_title('Spectrophotometric pH Measurements')
+ax.set_xlabel('Wavelength [nm]')
+ax.set_ylabel('Time [hr]')
+ax.set_zlabel('Absorbance')
+
+plt.show()
 
